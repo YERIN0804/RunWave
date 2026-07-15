f4dc14026486500af8a196e07b2d2167f9c4922b
jieun <qkdwldms9@naver.com>
2026-07-15 15:09:10 +0900

    글씨체 변경

--- src/App.vue
+++ src/App.vue
@@ -3,38 +3,5 @@ import HomeView from './views/HomeView.vue'
 </script>
 
 <template>
-<<<<<<< HEAD
   <HomeView />
-=======
-  <router-view />
-  <!-- <script setup>
-  import { ref, onMounted, onBeforeUnmount } from 'vue'
-  import BoardList from './components/BoardList.vue'
-  import BoardWrite from './components/BoardWrite.vue'
-  import BoardDetail from './components/BoardDetail.vue'
-  import BoardEdit from './components/BoardEdit.vue'
-
-  const currentHash = ref(location.hash || '#/')
-
-  function updateHash() {
-    currentHash.value = location.hash || '#/'
-  }
-
-  onMounted(() => {
-    window.addEventListener('hashchange', updateHash)
-  })
-
-  onBeforeUnmount(() => {
-    window.removeEventListener('hashchange', updateHash)
-  })
-  </script>
-
-  <template>
-    <main class="app">
-      <BoardWrite v-if="currentHash === '#/write'" />
-      <BoardDetail v-else-if="currentHash.startsWith('#/post/')" />
-      <BoardEdit v-else-if="currentHash.startsWith('#/edit/')" />
-      <BoardList v-else />
-    </main> -->
->>>>>>> 910b5e4708166eeeaf7dd93faa8e36e2cf39662d
 </template>
