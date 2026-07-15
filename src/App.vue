53cc828018001dfbc00a8f9dff8d98cce73ec606
yerin <dpfls0570@gmail.com>
2026-07-15 11:49:20 +0900

    지도 시각화 및 챗봇 머지

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+// import HelloWorld from './components/HelloWorld.vue'
 import RunChat from './components/RunChat.vue';
+import BeachMap from './components/BeachMap.vue';
+
+const touristSpots = ref([]) // 실제 데이터로 채울 경우 loadAllLocalData와 연동하세요
+
 </script>
 
 <template>
-  <HelloWorld />
-  <!-- <BeachMap :runningSpots="touristSpots" /> -->
+  <!-- <HelloWorld /> -->
+   <BeachMap :runningSpots="touristSpots" />
    <RunChat/>
 </template>
